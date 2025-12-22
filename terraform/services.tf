88839bfea37b164924b50bc36e4bb00ead6b0192
Ihor Dvoretskyi <ihor@linux.com>
2025-12-22 18:05:30 +0200

    refactor: Standardize formatting in Terraform files and set default project ID
    Signed-off-by: Ihor Dvoretskyi <ihor@linux.com>

--- terraform/services.tf
+++ terraform/services.tf
@@ -3,20 +3,20 @@
 
 resource "google_project_service" "required_apis" {
   for_each = toset([
-    "container.googleapis.com",       # GKE
-    "compute.googleapis.com",         # Compute Engine
-    "storage.googleapis.com",         # Cloud Storage
-    "bigquery.googleapis.com",        # BigQuery for cost tracking
-    "cloudbuild.googleapis.com",      # Cloud Build
-    "monitoring.googleapis.com",      # Cloud Monitoring
-    "logging.googleapis.com",         # Cloud Logging
+    "container.googleapis.com",            # GKE
+    "compute.googleapis.com",              # Compute Engine
+    "storage.googleapis.com",              # Cloud Storage
+    "bigquery.googleapis.com",             # BigQuery for cost tracking
+    "cloudbuild.googleapis.com",           # Cloud Build
+    "monitoring.googleapis.com",           # Cloud Monitoring
+    "logging.googleapis.com",              # Cloud Logging
     "cloudresourcemanager.googleapis.com", # Resource Manager
-    "iam.googleapis.com",             # IAM
+    "iam.googleapis.com",                  # IAM
   ])
 
   project = local.project_id
   service = each.value
 
-  disable_on_destroy = false
+  disable_on_destroy         = false
   disable_dependent_services = false
 }
